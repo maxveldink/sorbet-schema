efe7afb35908baf380337edf4a72d8fb8f5c48e9
Max VelDink <mveldink@justworks.com>
2024-02-16 15:34:03 -0500

    feat: Add TypeMismatchError

---
+++ lib/typed/type_mismatch_error.rb
@@ -0,0 +1,12 @@
+# typed: strict
+
+module Typed
+  class TypeMismatchError < ValidationError
+    extend T::Sig
+
+    sig { params(field_name: Symbol, field_type: T::Class[T.anything], given_type: T::Class[T.anything]).void }
+    def initialize(field_name:, field_type:, given_type:)
+      super("Invalid type given to #{field_name}. Expected #{field_type}, got #{given_type}.")
+    end
+  end
+end
---
+++ test/typed/type_mismatch_error_test.rb
@@ -0,0 +1,9 @@
+# typed: true
+
+require "test_helper"
+
+class TypeMismatchErrorTest < Minitest::Test
+  def test_formats_error_message
+    assert_equal("Invalid type given to im_required. Expected String, got Integer.", Typed::TypeMismatchError.new(field_name: :im_required, field_type: String, given_type: Integer).message)
+  end
+end
