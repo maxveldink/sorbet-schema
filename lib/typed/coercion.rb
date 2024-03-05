00583a1fd35bb41c08891dd3f2fc83dc0b455cc7
Max VelDink <mveldink@justworks.com>
2024-03-05 13:36:35 -0500

    feat: Add Integer coercion

--- lib/typed/coercion.rb
+++ lib/typed/coercion.rb
@@ -4,12 +4,18 @@ module Typed
   module Coercion
     extend T::Sig
 
+    # TODO: We can definitely improve how we select which coercer to use
+    #   Related issues:
+    #   * https://github.com/maxveldink/sorbet-schema/issues/9
+    #   * https://github.com/maxveldink/sorbet-schema/issues/10
     sig { type_parameters(:U).params(field: Field, value: Value).returns(Result[Value, CoercionError]) }
     def self.coerce(field:, value:)
       if field.type < T::Struct
         StructCoercer.coerce(field: field, value: value)
       elsif field.type == String
         StringCoercer.coerce(field: field, value: value)
+      elsif field.type == Integer
+        IntegerCoercer.coerce(field: field, value: value)
       else
         Failure.new(CoercionNotSupportedError.new)
       end
