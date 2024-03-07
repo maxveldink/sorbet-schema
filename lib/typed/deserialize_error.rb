bf5f770bc3ca176f18146dd780ad7ccd7fcb05b0
Max VelDink <mveldink@justworks.com>
2024-03-07 16:54:53 -0500

    feat: adds `to_h` and `to_json` methods to Deserialize errors (#28)

--- lib/typed/deserialize_error.rb
+++ lib/typed/deserialize_error.rb
@@ -2,5 +2,16 @@
 
 module Typed
   class DeserializeError < StandardError
+    extend T::Sig
+
+    sig { returns({error: String}) }
+    def to_h
+      {error: message}
+    end
+
+    sig { returns(String) }
+    def to_json
+      JSON.generate(to_h)
+    end
   end
 end
