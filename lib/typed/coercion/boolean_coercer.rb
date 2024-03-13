cf5c5ec7872bb4f4a2eb844b40a3fae01414308c
Max VelDink <mveldink@justworks.com>
2024-03-13 15:56:20 -0400

    feat: Add BooleanCoercer

---
+++ lib/typed/coercion/boolean_coercer.rb
@@ -0,0 +1,31 @@
+# typed: strict
+
+module Typed
+  module Coercion
+    class BooleanCoercer < Coercer
+      extend T::Generic
+
+      Target = type_member { {fixed: T::Boolean} }
+
+      sig { override.params(type: Field::Type).returns(T::Boolean) }
+      def used_for_type?(type)
+        type == T::Utils.coerce(T::Boolean)
+      end
+
+      sig { override.params(field: Field, value: Value).returns(Result[Target, CoercionError]) }
+      def coerce(field:, value:)
+        if T.cast(field.type, T::Types::Base).valid?(value)
+          Success.new(value)
+        elsif value == "true"
+          Success.new(true)
+        elsif value == "false"
+          Success.new(false)
+        else
+          Failure.new(CoercionError.new)
+        end
+      rescue TypeError => e
+        Failure.new(CoercionError.new("Field type must be a T::Boolean."))
+      end
+    end
+  end
+end
