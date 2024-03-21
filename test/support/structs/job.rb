59f3d1057db13edb654406927362a4df846a6191
Max VelDink <mveldink@justworks.com>
2024-03-21 10:11:30 -0400

    refactor: Match Job testing struct to other structs and add optional start date

--- test/support/structs/job.rb
+++ test/support/structs/job.rb
@@ -1,8 +1,14 @@
 # typed: true
 
+require "date"
+
 class Job < T::Struct
   include ActsAsComparable
 
   const :title, String
   const :salary, Integer
+  const :start_date, T.nilable(Date)
 end
+
+DEVELOPER_JOB = Job.new(title: "Software Developer", salary: 90_000_00)
+DEVELOPER_JOB_WITH_START_DATE = Job.new(title: "Software Developer", salary: 90_000_00, start_date: Date.new(2024, 3, 1))
