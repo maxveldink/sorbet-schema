5b542c64d71c58c9703ea13a1c10f82fd8acb3f1
Max VelDink <mveldink@justworks.com>
2024-08-03 19:54:52 -0400

    refactor!: Replace deeply_nested_hash with options passing in struct ext

--- lib/sorbet-schema/t/struct.rb
+++ lib/sorbet-schema/t/struct.rb
@@ -12,13 +12,11 @@ module T
         Typed::Schema.from_struct(self)
       end
 
-      sig { params(type: Symbol).returns(Typed::Serializer[T.untyped, T.untyped]) }
-      def serializer(type)
+      sig { params(type: Symbol, options: T::Hash[Symbol, T.untyped]).returns(Typed::Serializer[T.untyped, T.untyped]) }
+      def serializer(type, options: {})
         case type
-        when :deeply_nested_hash
-          Typed::HashSerializer.new(schema:, should_serialize_values: true)
         when :hash
-          Typed::HashSerializer.new(schema:)
+          Typed::HashSerializer.new(**T.unsafe({schema:, **options}))
         when :json
           Typed::JSONSerializer.new(schema:)
         else
@@ -26,15 +24,15 @@ module T
         end
       end
 
-      sig { params(serializer_type: Symbol, source: T.untyped).returns(Typed::Serializer::DeserializeResult) }
-      def deserialize_from(serializer_type, source)
-        serializer(serializer_type).deserialize(source)
+      sig { params(serializer_type: Symbol, source: T.untyped, options: T::Hash[Symbol, T.untyped]).returns(Typed::Serializer::DeserializeResult) }
+      def deserialize_from(serializer_type, source, options: {})
+        serializer(serializer_type, options:).deserialize(source)
       end
     end
 
-    sig { params(serializer_type: Symbol).returns(Typed::Result[T.untyped, Typed::SerializeError]) }
-    def serialize_to(serializer_type)
-      self.class.serializer(serializer_type).serialize(self)
+    sig { params(serializer_type: Symbol, options: T::Hash[Symbol, T.untyped]).returns(Typed::Result[T.untyped, Typed::SerializeError]) }
+    def serialize_to(serializer_type, options: {})
+      self.class.serializer(serializer_type, options:).serialize(self)
     end
   end
 end
