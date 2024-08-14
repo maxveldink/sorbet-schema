d36ddb1fc6ad03970ae9b276d8befd4697aad113
Max VelDink <mveldink@justworks.com>
2024-08-14 11:27:30 -0400

    fix: add sorbet-schema shim for better T:Struct extension compatability

--- lib/sorbet-schema/t/struct.rb
+++ lib/sorbet-schema/t/struct.rb
@@ -1,18 +1,12 @@
-# typed: strict
+# typed: true
 
 module T
   class Struct
-    extend T::Sig
-
     class << self
-      extend T::Sig
-
-      sig { overridable.returns(Typed::Schema) }
       def schema
         Typed::Schema.from_struct(self)
       end
 
-      sig { params(type: Symbol, options: T::Hash[Symbol, T.untyped]).returns(Typed::Serializer[T.untyped, T.untyped]) }
       def serializer(type, options: {})
         case type
         when :hash
@@ -24,13 +18,11 @@ module T
         end
       end
 
-      sig { params(serializer_type: Symbol, source: T.untyped, options: T::Hash[Symbol, T.untyped]).returns(Typed::Serializer::DeserializeResult) }
       def deserialize_from(serializer_type, source, options: {})
-        serializer(serializer_type, options:).deserialize(source)
+        T.unsafe(serializer(serializer_type, options:).deserialize(source))
       end
     end
 
-    sig { params(serializer_type: Symbol, options: T::Hash[Symbol, T.untyped]).returns(Typed::Result[T.untyped, Typed::SerializeError]) }
     def serialize_to(serializer_type, options: {})
       self.class.serializer(serializer_type, options:).serialize(self)
     end
